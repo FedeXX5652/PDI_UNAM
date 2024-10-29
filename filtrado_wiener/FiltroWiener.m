3e2d5eb281929414df5dbd26e355f4e65764a078
Ricardo Alonso Velasco Vanegas <goldersrockers@hotmail.com>
2024-10-28 20:24:02 -0600

    Add files via upload

--- filtrado_wiener/FiltroWiener.m
+++ filtrado_wiener/FiltroWiener.m
@@ -1,39 +1,41 @@
 % Cargar imagen sin ruido
-ImagenSinRuido = imread('Glaciar512.jpg'); 
+ImagenSinRuido_Espacial = imread('Glaciar512.jpg'); 
 
-if size(ImagenSinRuido, 3) == 3
-    ImagenSinRuido = rgb2gray(ImagenSinRuido);
+if size(ImagenSinRuido_Espacial, 3) == 3
+    ImagenSinRuido_Espacial = rgb2gray(ImagenSinRuido_Espacial);
 end
 
 % ----------------
 
-% Punto 1
-Sn = 0.01; % Varianza
-% Transformada
-ImagenSinRuidoFrec = fft2(double(ImagenSinRuido)); % Transformada de Fourier
+% Transformada de Fourier
+ImagenSinRuido_Frec = fft2(double(ImagenSinRuido_Espacial)); 
 
 % Densidad espectral de la imagen original
-PImagenSinRuido = abs(ImagenSinRuidoFrec).^2; 
+PImagenSinRuido = abs(ImagenSinRuido_Frec).^2; 
 
-% Generar ruido en el dominio de frecuencias
-ImagenConRuido = imnoise(zeros(size(ImagenSinRuido)), 'gaussian', 0.25, Sn); % Crear imagen de ruido
-ImagenConRuidoFrec = fft2(double(ImagenConRuido)); % Transformada de Fourier del ruido
+% Generar ruido en el dominio de frecuencias (aumentar la varianza del ruido)
+ImagenConRuido_Espacial = imnoise(ImagenSinRuido_Espacial, 'gaussian', 0.5);
+ImagenConRuido_Frec = fft2(double(ImagenConRuido_Espacial));
 
-% Imagen con ruido en el dominio de frecuencias
-ImagenConRuidoFrec = ImagenSinRuidoFrec + ImagenConRuidoFrec; 
+% Calcular la nueva varianza del ruido, evitar 0
+Sn = var(double(ImagenConRuido_Espacial(:))) + 1e-10;
 
-% Calcular la nueva varianza del ruido
-Sn = var(double(ImagenConRuido(:)));
+% Densidad espectral de la imagen con ruido (Para filtro wiener)
+PImagenConRuido = abs(ImagenConRuido_Frec).^2;
 
-% Densidad espectral de la imagen con ruido
-PImagenConRuido = abs(ImagenConRuidoFrec).^2; 
+% Filtro de Wiener
+WienerFiltro = PImagenSinRuido ./ (PImagenSinRuido + Sn); % Filtro de Wiener
 
-WienerFiltro = 1 ./ (1 + (Sn ./ PImagenConRuido)); % Filtro de Wiener
-
-ImagenFiltradaWienerFrec = ImagenConRuidoFrec .* WienerFiltro;
+% Aplicar el filtro Wiener a la imagen con ruido
+ImagenFiltradaWiener_Frec = ImagenConRuido_Frec .* WienerFiltro;
 
 % Transformada inversa para volver al dominio espacial
-ImagenFiltradaWiener = ifft2(ImagenFiltradaWienerFrec);
+ImagenFiltradaWiener_Espacial = ifft2(ImagenFiltradaWiener_Frec);
+
+% Tomar solo la parte real
+ImagenFiltradaWiener_Espacial = real(ImagenFiltradaWiener_Espacial);
+%Asegurar dentro de rango
+ImagenFiltradaWiener_Espacial = uint8(255 * mat2gray(ImagenFiltradaWiener_Espacial));
 
 % ----------------
 
@@ -41,98 +43,71 @@ ImagenFiltradaWiener = ifft2(ImagenFiltradaWienerFrec);
 % G = F . H (En dominio de frecuencias)
 
 % Filtro binomial 9x9 (Matriz H)
-FiltroBinomial = [1 8 28 56 70 56 28 8 1]' * [1 8 28 56 70 56 28 8 1];
-FiltroBinomial = FiltroBinomial / sum(FiltroBinomial(:)); % Normalizar
+FiltroBinomial_Espacial = [1 8 28 56 70 56 28 8 1]' * [1 8 28 56 70 56 28 8 1];
+FiltroBinomial_Espacial = FiltroBinomial_Espacial / sum(FiltroBinomial_Espacial(:)); % Normalizar
 
-% Transformada de Fourier de la imagen (Matriz F)
-ImagenFrec = fft2(double(ImagenSinRuido));
 % Transformada de Fourier del filtro (Matriz H)
-FiltroFrec = fft2(FiltroBinomial, 512, 512); % Padding del filtro
+Filtro_Frec = fft2(FiltroBinomial_Espacial, 512, 512); % Padding del filtro
 
 % Multiplicacion en dominio de frecuencias (Matriz G)
-ImagenFiltradaFrec = ImagenFrec .* FiltroFrec; 
+ImagenFiltrada_Frec = ImagenSinRuido_Frec .* Filtro_Frec; 
 
 % Transformada inversa para volver al dominio espacial
-ImagenFiltrada = ifft2(ImagenFiltradaFrec);
+ImagenFiltrada_Espacial = ifft2(ImagenFiltrada_Frec);
 
 % Calcular la matriz inversa del filtro 
 % en el dominio de la frecuencia (Matriz H^-1)
-FiltroFrecInverso = zeros(size(FiltroFrec));
+FiltroFrecInverso = zeros(size(Filtro_Frec));
 
-for i = 1:numel(FiltroFrec) % Evitar div entre 0
-    if FiltroFrec(i) ~= 0
-        FiltroFrecInverso(i) = 1 / FiltroFrec(i);
+for i = 1:numel(Filtro_Frec) % Evitar div entre 0
+    if Filtro_Frec(i) ~= 0
+        FiltroFrecInverso(i) = 1 / Filtro_Frec(i);
     end
 end
 
 % Producto de las matrices G y H^-1
-ProductoGHInverso = ImagenFiltradaFrec .* FiltroFrecInverso; 
+ImagenFiltroPasoBajasHInversa_Frec = ImagenFiltrada_Frec .* FiltroFrecInverso; 
 
 % Transformada inversa para volver al dominio espacial
-ImagenProductoGHInverso = ifft2(ProductoGHInverso);
-
-% ----------------
-
-% Punto 3: Imagen con ruido gaussiano y pérdida de nitidez
-
-% 1. Agregar ruido gaussiano a la imagen original
-ImagenConRuidoYDesenfoque = imnoise(ImagenSinRuido, 'gaussian', 0.25, Sn); % Usar el mismo Sn que se definió antes
-
-% 2. Filtrar la imagen con el filtro de paso bajo (Filtro binomial)
-ImagenConRuidoYDesenfoqueFrec = fft2(double(ImagenConRuidoYDesenfoque)); % Transformada de Fourier de la imagen con ruido
-ImagenFiltradaConRuidoYDesenfoqueFrec = ImagenConRuidoYDesenfoqueFrec .* FiltroFrec; % Multiplicación en el dominio de frecuencias
-
-% 3. Transformada inversa para obtener la imagen con ruido y desenfoque
-ImagenConRuidoYDesenfoqueFiltrada = ifft2(ImagenFiltradaConRuidoYDesenfoqueFrec);
-
-% 4. Calcular la nueva varianza del ruido en la imagen con ruido y desenfoque
-SnDesenfoque = var(double(ImagenConRuidoYDesenfoqueFiltrada(:)));
-
-% 5. Densidad espectral de la imagen con ruido y desenfoque
-PImagenConRuidoYDesenfoque = abs(ImagenFiltradaConRuidoYDesenfoqueFrec).^2; 
-
-% 6. Calcular el filtro de Wiener
-WienerFiltroDesenfoque = 1 ./ (1 + (SnDesenfoque ./ PImagenConRuidoYDesenfoque)); 
-
-% 7. Filtrar la imagen con el filtro de Wiener
-ImagenFiltradaWienerDesenfoqueFrec = ImagenFiltradaConRuidoYDesenfoqueFrec .* WienerFiltroDesenfoque;
-
-% 8. Transformada inversa para volver al dominio espacial
-ImagenFiltradaWienerDesenfoque = ifft2(ImagenFiltradaWienerDesenfoqueFrec);
+ImagenFiltroPasoBajasHInversa_Espacial = ifft2(ImagenFiltroPasoBajasHInversa_Frec);
 
 % ----------------
 
 % Resultados
 figure;
-imshow(uint8(ImagenSinRuido));
+imshow(uint8(ImagenSinRuido_Espacial));
 title('Imagen Sin Ruido');
 
 figure;
-imshow(log(1 + abs(fftshift(ImagenFrec))), []);
+imshow(uint8(ImagenConRuido_Espacial));
+title('Imagen Con Ruido añadido');
+
+figure;
+imshow(log(1 + abs(fftshift(ImagenSinRuido_Frec))), []);
 title('Imagen Sin Ruido en frecuencias (Punto 1)');
 
 figure;
-imshow(log(1 + abs(fftshift(ImagenConRuidoFrec))), []);
+imshow(log(1 + abs(fftshift(ImagenConRuido_Frec))), []);
 title('Imagen en frecuencias con ruido añadido (Punto 1)');
 
 figure;
-imshow(uint8(real(ImagenFiltradaWiener)));
+imshow(uint8(real(ImagenFiltradaWiener_Espacial)));
 title('Imagen filtrada con reducción de ruido (Filtro de Wiener) (Punto 1)');
 
 figure;
-imshow(log(1 + abs(fftshift(ImagenFrec))), []);
+imshow(log(1 + abs(fftshift(ImagenSinRuido_Frec))), []);
 title('Imagen en frecuencias sin ruido (Punto 2)');
 
 figure;
-imshow(log(1 + abs(fftshift(FiltroFrec))), []);
+imshow(log(1 + abs(fftshift(Filtro_Frec))), []);
 title('Filtro en frecuencias (Punto 2)');
 
 figure;
-imshow(log(1 + abs(fftshift(ImagenFiltradaFrec))), []);
+imshow(log(1 + abs(fftshift(ImagenFiltrada_Frec))), []);
 title('Imagen filtrada en frecuencias (Punto 2)');
 
 figure;
-imshow(uint8(ImagenFiltrada));
+imshow(uint8(ImagenFiltrada_Espacial));
 title('Imagen filtrada (Punto 2)');
 
 figure;
@@ -140,13 +115,9 @@ imshow(log(1 + abs(fftshift(FiltroFrecInverso))), []);
 title('Filtro Inverso en Frecuencias (Punto 2)');
 
 figure;
-imshow(log(1 + abs(fftshift(ProductoGHInverso))), []);
+imshow(log(1 + abs(fftshift(ImagenFiltroPasoBajasHInversa_Frec))), []);
 title('Producto G * H^{-1} en Frecuencias (Punto 2)');
 
 figure;
-imshow(uint8(ImagenProductoGHInverso));
+imshow(uint8(ImagenFiltroPasoBajasHInversa_Espacial));
 title('Imagen Producto G * H^{-1} (Punto 2)');
-
-figure;
-imshow(uint8(real(ImagenFiltradaWienerDesenfoque)));
-title('Imagen restaurada con Wiener (Punto 3)');
